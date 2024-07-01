32a0b7a656f7c4b0351a00b022c2df16fbb2f0df
sammie slootman <sammie.slootman@gmail.com>
2024-07-01 14:57:11 +0200

    changed the search border color with dark/lighmode, also started with the animation for the placeholder text when focused

--- flag-app/src/components/Search.jsx
+++ flag-app/src/components/Search.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import "./Search.css";
 
-
 const Search = ({ SearchQuerry, setSearchQuerry }) => {
   return (
     <div className="search-container">
+      <label className={`search-placeholder ${SearchQuerry ? 'active' : ''}`} htmlFor="search-input">
+        Search for a country
+      </label>
       <input
-      type="text"
-      placeholder="Search for a country..."
-      value={SearchQuerry}
-      onChange={(e) => setSearchQuerry(e.target.value)}
-    />
+        id="search-input"
+        type="text"
+        value={SearchQuerry}
+        onChange={(e) => setSearchQuerry(e.target.value)}
+        onFocus={(e) => e.target.previousSibling.classList.add('active')}
+        onBlur={(e) => {
+          if (!e.target.value) {
+            e.target.previousSibling.classList.remove('active');
+          }
+        }}
+      />
     </div>
   );
 };
